e39c76e8964a2c20c6896ffbcbb516781e67c970
“MrSorrow <Kingdompin@163.com>
2018-06-18 17:35:04 +0800

    维护具体商品时根据分类id查询规格参数模板

--- taotao-manager/taotao-manager-web/src/main/webapp/WEB-INF/js/common.js
+++ taotao-manager/taotao-manager-web/src/main/webapp/WEB-INF/js/common.js
@@ -190,7 +190,7 @@ var TT = TAOTAO = {
     },
     
     changeItemParam : function(node,formId){
-    	$.getJSON("/item/param/cid/" + node.id,function(data){
+    	$.getJSON("/item/param//query/itemcatid/" + node.id,function(data){
 			  if(data.status == 200 && data.data){
 				 $("#"+formId+" .params").show();
 				 var paramData = JSON.parse(data.data.paramData);
